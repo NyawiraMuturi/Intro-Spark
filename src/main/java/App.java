4466508b756d129edad3c61f162c447a31998bd3
Nyawira Muturi <albinamuturi@gmail.com>
2020-04-23 16:06:03 +0300

    add velocity features

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 
@@ -10,15 +11,13 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/", (request, response) ->{
-
-            return new ModelAndView(new HashMap(), "hello.hbs");
-
-        }, new HandlebarsTemplateEngine());
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "templates/hello.vtl");
+        }, new VelocityTemplateEngine());
 
         get("/favorite_photos", (request, response) -> {
-            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
-        }, new HandlebarsTemplateEngine());
+            return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
+        }, new VelocityTemplateEngine());
 
     }
 }
